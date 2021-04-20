9bb1f5c24142e01ed53182e65f16e854116a8ad8
NidhiBhandiwad <nidhi.sanjeevb@gmail.com>
2021-04-21 11:45:02 +1200

    sign up test added

---
+++ registerbackend/tests/signup.test.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+describe('user CRUD',() => {
+    let connection;
+    
+    const user = mongoose.model("test_users",mongoose.Schema({
+        name: String,
+        username: String,
+        password: String
+    }));
+    beforeAll(async () => {
+       
+        connection = await mongoose.connect('mongodb+srv://powerpuffgirls:PowerPuffGirls@cluster0.6hhuy.mongodb.net/seeds_testing?retryWrites=true&w=majority');
+        db = mongoose.connection;
+
+    });
+    
+    test("sign up user",async () => {
+
+        const response = await user.create({
+            name: "test",
+            username: "test_username",
+            password: "test_password"
+        });
+        await response.save();
+        expect(response.name).toBe("test");
+        
+
+    });
+
+    
+ });
