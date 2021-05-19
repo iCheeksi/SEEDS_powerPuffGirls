50c2f1eda075028c4981eaac0e147f92d1f529d5
NidhiBhandiwad <nidhi.sanjeevb@gmail.com>
2021-05-19 22:19:50 +1200

    heroku added and fixed

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const routes = require('./router/api')
 dotenv.config()
 const MONGODB = "mongodb+srv://powerpuffgirls:PowerPuffGirls@cluster0.6hhuy.mongodb.net/userTable?retryWrites=true&w=majority";
 
+
 mongoose.connect(process.env.MONGODB_URI || MONGODB), {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -21,10 +22,9 @@ app.use(cor())
 app.use('/api', routes)
 app.use('/server', routeUrls)
 
+//for heroku deployment
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('register/build'));
-    
-    
+    app.use(express.static('register/build')); 
   }
 
 app.listen( process.env.PORT || 3001, () =>{
