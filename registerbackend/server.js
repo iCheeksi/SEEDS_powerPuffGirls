f386f56e1491ddd1abb252c8787fa2f2cdd0590d
NidhiBhandiwad <nidhi.sanjeevb@gmail.com>
2021-05-19 19:55:09 +1200

    trying to add heroku

--- registerbackend/server.js
+++ registerbackend/server.js
@@ -5,6 +5,7 @@ var mongoose = require('mongoose')
 var dotenv = require("dotenv")
 var routeUrls = require('./router/route')
 const routes = require('./router/api')
+
 dotenv.config()
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = "mongodb+srv://powerpuffgirls:PowerPuffGirls@cluster0.6hhuy.mongodb.net/userTable?retryWrites=true&w=majority";
@@ -21,6 +22,14 @@ app.use(express.json()) //body passer activated
 app.use(cor())
 app.use('/api', routes)
 app.use('/server', routeUrls)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../register/build")));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../register/build/index.html")); // relative path
+    });
+  }
+
 app.listen(PORT || 3001, () =>{
     console.log("Server is working!")
 })
